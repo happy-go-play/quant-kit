888317649e903ed7f0051f23dfc855fe5f26e2e5
taobig <studentht@hotmail.com>
2025-09-02 21:37:50 +0800

    Add decimal constants for common values

---
+++ vars.go
@@ -0,0 +1,30 @@
+package quantkit
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+// var ZeroDecimal = decimal.Zero // 请直接使用 decimal.Zero
+
+var OneDecimal = decimal.NewFromInt(1)
+var TwoDecimal = decimal.NewFromInt(2)
+var ThreeDecimal = decimal.NewFromInt(3)
+var FourDecimal = decimal.NewFromInt(4)
+var FiveDecimal = decimal.NewFromInt(5)
+var SixDecimal = decimal.NewFromInt(6)
+var SevenDecimal = decimal.NewFromInt(7)
+var EightDecimal = decimal.NewFromInt(8)
+var NineDecimal = decimal.NewFromInt(9)
+var TenDecimal = decimal.NewFromInt(10)
+var HundredDecimal = decimal.NewFromInt(100)
+var ThousandDecimal = decimal.NewFromInt(1000)
+var TenThousandDecimal = decimal.NewFromInt(10_000)
+var MillionDecimal = decimal.NewFromInt(1_000_000)
+var BillionDecimal = decimal.NewFromInt(1_000_000_000)
+
+//var OneHalfDecimal = decimal.NewFromFloat(0.5)
+//var OneQuarterDecimal = decimal.NewFromFloat(0.25)
+//var OneTenthDecimal = decimal.NewFromFloat(0.1)
+//var OneHundredthDecimal = decimal.NewFromFloat(0.01)
+//
+//var MinusOneDecimal = decimal.NewFromInt(-1)
